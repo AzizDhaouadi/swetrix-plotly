c2181dab88c8df5b258c4d8f3cba35340c7ed8bf
Aziz Dhaouadi <dhaziz.works@gmail.com>
2024-02-18 02:14:24 -0500

    Added imports for traffic breakdown viz

--- public/js/renderTrafficData.js
+++ public/js/renderTrafficData.js
@@ -3,6 +3,10 @@
 import { renderCustomEventsData } from "./dashboardVisualizations/customEventsData.js";
 import { renderTrafficOvertime } from "./dashboardVisualizations/trafficOverview.js";
 import { renderPagesAndReferrersData } from "./dashboardVisualizations/pagesAndRefferers.js";
+import { renderTrafficGeoBreakdownData } from "./dashboardVisualizations/trafficGeoBreakdown.js";
+import { renderTrafficDeviceBreakdownData } from "./dashboardVisualizations/trafficDeviceBreakdown.js";
+import { renderTrafficBrowserBreakdownData } from "./dashboardVisualizations/trafficBrowserBreakdown.js";
+import { renderTrafficOsBreakdownData } from "./dashboardVisualizations/trafficOsBreakdown.js";
 
 const fetchTrafficData = async () => {
   const fetchTrafficDataRequest = await fetch("/fetch/trafficData");
@@ -17,6 +21,10 @@ const rendertrafficData = async () => {
   renderTrafficOvertime(trafficData);
   renderCustomEventsData(trafficData);
   renderPagesAndReferrersData(trafficData);
+  renderTrafficGeoBreakdownData(trafficData);
+  renderTrafficDeviceBreakdownData(trafficData);
+  renderTrafficBrowserBreakdownData(trafficData);
+  renderTrafficOsBreakdownData(trafficData);
 };
 
 rendertrafficData();
