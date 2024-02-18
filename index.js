af2d6884d697c0d95a5c30ca7554f183b09984ea
Aziz Dhaouadi <dhaziz.works@gmail.com>
2024-02-18 01:06:09 -0500

    Added the route for fetching performance data

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const port = process.env.PORT;
 // Import fetch from node-fetch
 const fetch = (...args) => import("node-fetch").then(({ default: fetch }) => fetch(...args));
 
-// Defining route to fetch data for custom events
+// Defining route to fetch data for traffic
 app.get("/fetch/trafficData", async (req, res) => {
   const baseUrl = "https://api.swetrix.com/v1/log";
   const projectID = process.env.PROJECTID;
@@ -25,10 +25,36 @@ app.get("/fetch/trafficData", async (req, res) => {
     });
 
     if (trafficDataRequest.ok) {
-      res.json(trafficDataRequest);
+      const trafficData = await trafficDataRequest.json();
+      res.json(trafficData);
     }
   } catch (error) {
     console.error("Failedf to fetch traffic data", error);
+    res.status(500).json({ message: "Failed to fetch external data" });
+  }
+});
+
+// Defining route to fetch data for performance
+app.get("/fetch/performanceData", async (req, res) => {
+  const baseUrl = "https://api.swetrix.com/v1/log/performance";
+  const projectID = process.env.PROJECTID;
+  const period = "4w";
+  const timeBucket = "day";
+  const urlToFetch = `${baseUrl}?pid=${projectID}&period=${period}&timeBucket=${timeBucket}`;
+
+  try {
+    const performanceDataRequest = await fetch(urlToFetch, {
+      headers: {
+        "x-api-key": process.env.APIKEY,
+      },
+    });
+
+    if (performanceDataRequest.ok) {
+      const performanceData = await performanceDataRequest.json();
+      res.json(performanceData);
+    }
+  } catch (error) {
+    console.error("Failed to fetch traffic data", error);
     res.status(500).json({ message: "Failed to fetch external data" });
   }
 });
