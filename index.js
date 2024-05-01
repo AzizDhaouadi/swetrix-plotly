d6f3de0a918236684300e6a3b82eed002c949e7a
Aziz Dhaouadi <dhaziz.works@gmail.com>
2024-04-30 23:43:34 -0400

    Added POST route towards openAI

--- index.js
+++ index.js
@@ -2,12 +2,18 @@ require("dotenv").config();
 
 const express = require("express");
 const path = require("path");
-
+const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT;
 
+const OpenAI = require("openai");
+const openai = new OpenAI(process.env.ORGANIZATION_ID);
+
 // Import fetch from node-fetch
 const fetch = (...args) => import("node-fetch").then(({ default: fetch }) => fetch(...args));
+
+// Parse raw text
+app.use(bodyParser.text({ type: "text/plain" }));
 
 // Defining route to fetch data for traffic
 app.get("/fetch/trafficData", async (req, res) => {
@@ -80,6 +86,27 @@ app.get("/fetch/trafficMetricsData", async (req, res) => {
   } catch (error) {
     console.error("Failed to fetch traffic data", error);
     res.status(500).json({ message: "Failed to fetch external data" });
+  }
+});
+
+app.post("/analyze", async (req, res) => {
+  const messages = [
+    { role: "system", content: "You are a helpful assistant" },
+    { role: "user", content: `Analyze and offer insights about the following data: ${req.body}.` },
+  ];
+  const model = "gpt-4-turbo-preview";
+
+  try {
+    const completion = await openai.chat.completions.create({
+      messages: messages,
+      model: model,
+    });
+    if (typeof completion === "undefined") {
+      res.send("Failed to fait the translation");
+    }
+    res.json(completion?.choices[0].message?.content);
+  } catch (error) {
+    console.error(error);
   }
 });
 
